4ea28a03b4fdf5c4b34ed7a28362d9e01cb4c173
Alison Magid Juma <alisonmagid2016@gmail.com>
2022-04-06 13:02:19 +0000

    he will findout the issue soon

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
-import userRouter from "./routes/users.js"
+import userRouter from "./routes/users.js";
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use("/posts", postRoutes);
-app.use("/", (req, res)=>{
-  res.send("welcome to y2y organization")
-});
 app.use("/user", userRouter);
 const PORT = process.env.PORT || 5000;
 mongoose
