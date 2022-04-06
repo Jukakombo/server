ece54e052f472ea562396ac65cef7e21fa06a4b0
Alison Magid Juma <alisonmagid2016@gmail.com>
2022-04-06 11:57:30 +0000

    he will be the best

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use("/posts", postRoutes);
+app.use("/", (req, res)=>{
+  res.send("welcome to y2y organization")
+});
 app.use("/user", userRouter);
 const PORT = process.env.PORT || 5000;
 mongoose
