3ec9476a65b591ca710bcad2fc712774cee8a79c
Alison Magid Juma <alisonmagid2016@gmail.com>
2022-04-16 22:36:30 +0000

    see him never give up

--- models/registration.js
+++ models/registration.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 const registrationSchema = mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    // user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     firstName: {type:String, required: true},
     lastName: {type:String, required: true},
     correctEmail: {type:String, required: true},
     city: {type:String, required: true},
     delegate: {type:String, required: true},
     country: {type:String, required: true},
-    amount: Number,
+    amount: {type:Number , required: true},
     paymentMethode: {type:String, required: true},
     paymentStatus: {type:String, required: true},
     whatsApp: {type:String, required: true},
-    paidAt: {type:Date},
   },
   { timestamps: true }
 );
